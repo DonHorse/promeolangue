fd829a520deb3ff5296e2f1d3008356a74f4867d
DonHorse <73821367+DonHorse@users.noreply.github.com>
2021-08-22 22:41:47 +0200

    22 08 21

--- src/Questionnaire.js
+++ src/Questionnaire.js
@@ -1,8 +1,43 @@
-import React from "react";
+// Formulaire de connexion à une session
+// imports des librairies
+
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import Axios from "axios";
 
+// fonction contenant le formulaire de login
 function Questionnaire(){
+    const [Question, setQuestion] = useState("");
+    const [Reponse1, setReponse1] = useState("");
+    const [Reponse2, setReponse2] = useState("");
+    const [Reponse3, setReponse3] = useState("");
+    const [Reponse4, setReponse4] = useState("");
+    const [Correction, setCorrection] = useState();
 
+    const [Qid , setQid] = useState();
+
+    // requête à l'API  (GET l'id du dernier formulaire, POST les questions)
+    useEffect(() => {
+        Axios.get("http://localhost:3001/api/getLastForm").then((response) => {
+            setQid(response.data[0].id);
+        });
+    });
+
+    const addQuestion = () =>{
+        Axios.post("http://localhost:3001/api/newQuestion", {
+            question : Question,
+            reponse1 : Reponse1,
+            reponse2 : Reponse2,
+            reponse3 : Reponse3,
+            reponse4 : Reponse4,
+            correction : Correction,
+            Qid : Qid,
+        }).then(() => {
+            alert("Question enregistré !")
+        });
+    };
+
+    // Affichage du formulaire
     let {slug} = useParams()
 
     return(
